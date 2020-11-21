63aeebe7c587d0ab19bf8e6be2f88356488df722
Kj McCray <kensvne@Kjs-MacBook-Pro.local>
2020-11-20 20:36:02 -0500

    Update

--- Medium(3)/Medium.js
+++ Medium(3)/Medium.js
@@ -6,3 +6,12 @@
 // Example 2:
 // Input: gdc_two_numbers(78,126)
 // Output: 6
+
+function gcd(num1, num2) {
+    if (num2 === 0) {
+        return num1
+    }
+    return gcd(num2, num1 % num2)
+};
+
+console.log(gcd(num1, num2));
